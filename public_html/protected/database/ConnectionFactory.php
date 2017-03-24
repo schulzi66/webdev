a3cec42fac932cf76c1629a3be5db9a384fc05e9
Julian <julian.christer@web.de>
2017-03-24 10:05:17 +0000

    Database impl

--- public_html/protected/database/ConnectionFactory.php
+++ public_html/protected/database/ConnectionFactory.php
@@ -1,23 +1,43 @@
 <?php
 
 namespace Database\ConnectionFactory;
+
+use PDO;
+use PDOException;
+
 /**
  * Created by PhpStorm.
  * User: Julian
  * Date: 24/03/2017
  * Time: 9:37 AM
  */
-class ConnectionFactory {
+class ConnectionFactory
+{
     protected static $connection;
 
 
-    public function getConnection() {
-        if(self::$connection) {
-            self::$connection = new PDO("mysql:");
+    public function getConnection()
+    {
+        if (self::$connection) {
+            try {
+                $dbConfig = $this->loadConfiguration();
+                $user = "";
+                $password = "";
+
+                self::$connection = new PDO($dbConfig, $user, $password );
+            } catch(PDOException $e) {
+                echo 'Connection failed ' . $e->getMessage();
+            }
         }
     }
 
-    private function loadConfiguration($file = 'database.ini') {
-        //TODO
+    private function loadConfiguration($file = 'database.ini')
+    {
+        $settings = parse_ini_file($file, TRUE);
+
+        $dbConfig = $settings['database']['driver'] . ':host=' . $settings['database']['host'] .
+            ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
+            ';dbname=' . $settings['database']['schema'];
+        return $dbConfig;
     }
 }
