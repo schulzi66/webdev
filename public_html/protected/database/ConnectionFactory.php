0819d430c3918b28ebdb06bfa639d39d58c81a6c
Darlith <rezzikoep@web.de>
2017-04-04 16:18:02 +0100

    ghost writer

--- public_html/protected/database/ConnectionFactory.php
+++ public_html/protected/database/ConnectionFactory.php
@@ -5,12 +5,6 @@ namespace Database\ConnectionFactory;
 use Exception;
 use mysqli;
 
-/**
- * Created by PhpStorm.
- * User: Julian
- * Date: 24/03/2017
- * Time: 9:37 AM
- */
 class ConnectionFactory
 {
     protected static $connection;
