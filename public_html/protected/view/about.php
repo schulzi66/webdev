0e75e31779dcda11646f89c10fe9156717366689
Julian <julian.christer@web.de>
2017-04-05 14:39:53 +0100

    includes for all standard viewsC

--- public_html/protected/view/about.php
+++ public_html/protected/view/about.php
@@ -1,1 +1,12 @@
-<?php
+<html>
+<head>
+    <?php include '../protected/view/parts/head.php'; ?>
+</head>
+<?php include '../protected/view/parts/header.php'; ?>
+<body>
+
+<!-- Keep this at the end of the body tag to load the scripts at the right time -->
+<?php include '../protected/view/parts/scripts.php'; ?>
+</body>
+<?php include '../protected/view/parts/footer.php'; ?>
+</html>
