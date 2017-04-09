f10dac0b5091986f866f3708ceb23169245eedad
Julian <julian.christer@web.de>
2017-04-09 22:11:30 +0100

    fix, styles

--- public_html/protected/view/admin.php
+++ public_html/protected/view/admin.php
@@ -1,37 +1,39 @@
 <html>
 <head>
-    <?php include '../protected/view/parts/head.php'; ?>
+    <?php include '../../protected/view/parts/head.php'; ?>
 </head>
-<?php include '../protected/view/parts/header.php'; ?>
+<?php include '../../protected/view/parts/header.php'; ?>
 <body>
-<div class="outer col" data-placeholder-label="Header">
-    <div class="mainSearchWrapper">
-        <div class="outer col">
+<div class="" data-placeholder-label="Header">
+    <div class="">
+        <div class="">
             <div class="heading"><h1>SWD LIBRARY</h1>
             </div>
         </div>
         <form action="../action/admin.php" method="post">
-            <fieldset class="fieldset">
-                <legend>Admin Login:</legend>
-                <p>
-                    <label>User name:* </label>
-                    <input type="text" name="userName" reguired />
-                </p>
-                <p>
-                    <label>Password:* </label>
-                    <input type="password" name="password" required>
-                </p>
-                <p>
-                    <input class="submit" value="Login" type="submit">
-                </p>
-            </fieldset>
+            <div class="form-group">
+                <fieldset class="fieldset">
+                    <legend>Admin Login:</legend>
+                    <p>
+                        <label>User name:* </label>
+                        <input type="text" name="userName" reguired/>
+                    </p>
+                    <p>
+                        <label>Password:* </label>
+                        <input type="password" name="password" required>
+                    </p>
+                    <p>
+                        <input class="submit" value="Login" type="submit">
+                    </p>
+                </fieldset>
+            </div>
         </form>
     </div>
 </div>
 <!-- Keep this at the end of the body tag to load the scripts at the right time -->
-<?php include '../protected/view/parts/scripts.php'; ?>
+<?php include '../../protected/view/parts/scripts.php'; ?>
 </body>
-<?php include '../protected/view/parts/footer.php'; ?>
+<?php include '../../protected/view/parts/footer.php'; ?>
 </html>
 
 
--- public_html/protected/view/parts/head.php
+++ public_html/protected/view/parts/head.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <?php
-if(basename($_SERVER['PHP_SELF']) == "index") {
+if(basename($_SERVER['PHP_SELF']) == "index.php") {
 ?>
     <link rel="stylesheet" href="../src/css/main.css">
 <?php
