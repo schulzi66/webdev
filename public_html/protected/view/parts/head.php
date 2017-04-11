03ab3f60e95fd71daed0ed54734d7e13ae539064
Schulzi66 <marius.schulze@t-online.de>
2017-04-11 16:42:56 +0100

    FUCK JULIAN FOR FECKING HARDCODING THE FUCKING PORT NUMBER IN THE FUCKING HEAD.PHP SCRIPT!!!

--- public_html/protected/view/parts/head.php
+++ public_html/protected/view/parts/head.php
@@ -1,4 +1,4 @@
-<base href="http://localhost:63342/Webdev/public_html/src/">
+<base href="http://localhost/Webdev/public_html/src/">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <?php
