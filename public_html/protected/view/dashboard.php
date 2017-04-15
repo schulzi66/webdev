c04340b6491127fab543e1bb521daf3996f119d4
Julian <julian.christer@web.de>
2017-04-15 16:49:37 +0200

    initial dashboard

--- public_html/protected/view/dashboard.php
+++ public_html/protected/view/dashboard.php
@@ -5,9 +5,9 @@ SessionController::validateAdminSession();
 ?>
 <html>
 <head>
-    <?php include '../protected/view/parts/head.php'; ?>
+    <?php include '../../protected/view/parts/head.php'; ?>
 </head>
-<?php include '../protected/view/parts/header.php'; ?>
+<?php include '../../protected/view/parts/header.php'; ?>
 <body>
 <div class="" data-placeholder-label="Header">
     <div class="">
@@ -26,10 +26,104 @@ SessionController::validateAdminSession();
         </div>
     </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Dashboard</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-comments fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="admin-panel-text">Emails</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer panel-footer-blue">
+                        <span class="pull-left">Jetzt abrufen</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-shopping-cart fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="admin-panel-text">Bestellungen</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left">Jetzt bestätigen</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-red">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-upload fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="admin-panel-text">Rezept hochladen</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer panel-footer">
+                        <span class="pull-left">Jetzt hochladen</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-user-plus fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="admin-panel-text">Mitarbeiter hinzufügen</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left">Jetzt anlegen</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Keep this at the end of the body tag to load the scripts at the right time -->
-<?php include '../protected/view/parts/scripts.php'; ?>
+<?php include '../../protected/view/parts/scripts.php'; ?>
 </body>
-<?php include '../protected/view/parts/footer.php'; ?>
+<?php include '../../protected/view/parts/footer.php'; ?>
 </html>
 
 
