45738d164b7a77caa872aa1283f3889d1e08a966
Julian <julian.christer@web.de>
2017-04-17 16:59:24 +0200

    bugfix

--- sql/CreateDatabase.sql
+++ sql/CreateDatabase.sql
@@ -23,28 +23,32 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 INSERT INTO books
-VALUES(1,"Harry Potter and the Chamber of Secrets", "J.K. Rowling", "0439064872", "Children,Fantasy", NULL, NULL, NULL),
-  (2,"A Feast of Crows", "George R.R. Martin", "055358202X", "Fantasy", NULL, NULL, NULL),
-  (3,"A Dance with Dragons", "George R.R. Martin", "0553841122", "Fantasy", NULL, NULL, NULL),
-  (4,"A Game of Thrones", "George R.R. Martin", "0553573403", "Fantasy", NULL, NULL, NULL),
-  (5,"Das Kaenguru-Manifest", "Marc-Uwe Kling", "3548373836", "Humour,Political", NULL, NULL, NULL),
-  (6,"Relativity: The Special and the General Theory", "Albert Einstein", "1891396307", "Science,Education", NULL, NULL, NULL),
-  (7,"Eine Billion Dollar", "Andreas Eschbach", "3404150406", "Thriller,Fantasy", NULL, NULL, NULL),
-  (8,"The Call of Cthulhu", "H.P. Lovecraft", "1500584339", "Horror,Fantasy", NULL, NULL, NULL),
-  (9,"Paperback Oxford English Dictionary", "Oxford Dictionaries", "0199640947", "Education", NULL, NULL, NULL),
-  (10,"Gone Girl: A Novel", "Gillian Flynn", "0385347774", "Mystery,Thriller", NULL, NULL, NULL),
-  (11,"How To Win Friends And Influence People", "Dale Carnegie", "1439199191", "Business", NULL, NULL, NULL ),
-  (12,"The 4-Hour Workweek", "Timothy Ferriss", "0307465357", "Business", NULL, NULL, NULL),
-  (13,"Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "0099590085", "History,Education", NULL, NULL, NULL),
+VALUES
+  (1, "Harry Potter and the Chamber of Secrets", "J.K. Rowling", "0439064872", "Children,Fantasy", NULL, NULL, NULL),
+  (2, "A Feast of Crows", "George R.R. Martin", "055358202X", "Fantasy", NULL, NULL, NULL),
+  (3, "A Dance with Dragons", "George R.R. Martin", "0553841122", "Fantasy", NULL, NULL, NULL),
+  (4, "A Game of Thrones", "George R.R. Martin", "0553573403", "Fantasy", NULL, NULL, NULL),
+  (5, "Das Kaenguru-Manifest", "Marc-Uwe Kling", "3548373836", "Humour,Political", NULL, NULL, NULL),
+  (6, "Relativity: The Special and the General Theory", "Albert Einstein", "1891396307", "Science,Education", NULL, NULL, NULL),
+  (7, "Eine Billion Dollar", "Andreas Eschbach", "3404150406", "Thriller,Fantasy", NULL, NULL, NULL),
+  (8, "The Call of Cthulhu", "H.P. Lovecraft", "1500584339", "Horror,Fantasy", NULL, NULL, NULL),
+  (9, "Paperback Oxford English Dictionary", "Oxford Dictionaries", "0199640947", "Education", NULL, NULL, NULL),
+  (10, "Gone Girl: A Novel", "Gillian Flynn", "0385347774", "Mystery,Thriller", NULL, NULL, NULL),
+  (11, "How To Win Friends And Influence People", "Dale Carnegie", "1439199191", "Business", NULL, NULL, NULL),
+  (12, "The 4-Hour Workweek", "Timothy Ferriss", "0307465357", "Business", NULL, NULL, NULL),
+  (13, "Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "0099590085", "History,Education", NULL, NULL,
+   NULL),
   (14, "Java ist auch eine Insel", "Christian Ullenboom", "3836241196", "Education", NULL, NULL, NULL),
   (15, "Harry Potter and the Philosopher's Stone", "J.K. Rowling", "1408855658", "Children,Fantasy", NULL, NULL, NULL),
   (16, "The Lord of the Rings", "J.R.R. Tolkien", "0007525540", "Fantasy", NULL, NULL, NULL),
   (17, "Burnt Paper Sky", "Gilly Macmillan", "0349406391", "Mystery,Thriller", NULL, NULL, NULL),
   (18, "The Da Vinci Code", "Dan Brown", "0307474275", "Mystery,Thriller", NULL, NULL, NULL),
-  (19, "The Dictator's Handbook: Why Bad Behavior is Almost Always Good Politics", "Bruce Bueno de Mesquita", "1610391845", "History,Education", NULL, NULL, NULL),
+  (19, "The Dictator's Handbook: Why Bad Behavior is Almost Always Good Politics", "Bruce Bueno de Mesquita",
+   "1610391845", "History,Education", NULL, NULL, NULL),
   (20, "The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "0345391802", "Fantasy,Humour", NULL, NULL, NULL);
 
-ALTER TABLE books AUTO_INCREMENT = 21;
+ALTER TABLE books
+  AUTO_INCREMENT = 21;
 
 DROP TABLE IF EXISTS member;
 CREATE TABLE IF NOT EXISTS member (
@@ -58,13 +62,15 @@ CREATE TABLE IF NOT EXISTS member (
   Email     VARCHAR(50)
 );
 INSERT INTO member
-VALUES(1,"Philip", "Koep", "School Avenue, Glasheen, Cork", "+4901721234567", "1992-01-23", "Male", "philip.koep@eufh-mail.de"),
-  (2,"Marius","Schulze", "School Avenue, Glasheen, Cork", "+4901607654321", "1995-04-01", "Male", "marius.schulze@eufh-mail.de");
+VALUES (1, "Philip", "Koep", "School Avenue, Glasheen, Cork", "+4901721234567", "1992-01-23", "Male",
+        "philip.koep@eufh-mail.de"),
+  (2, "Marius", "Schulze", "School Avenue, Glasheen, Cork", "+4901607654321", "1995-04-01", "Male",
+   "marius.schulze@eufh-mail.de");
 
 ALTER TABLE books
-  Add Constraint FK_books_member
-FOREIGN KEY(MemberID)
-REFERENCES member(MemberID);
+  ADD CONSTRAINT FK_books_member
+FOREIGN KEY (MemberID)
+REFERENCES member (MemberID);
 
 UPDATE books
 SET MemberID = 1, Taken = "2017-04-17"
@@ -78,7 +84,7 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users
-VALUES(0, "admin", "password");
+VALUES (0, "admin", "password");
 
 DROP TABLE IF EXISTS images;
 CREATE TABLE IF NOT EXISTS images (
@@ -94,33 +100,35 @@ DROP TABLE IF EXISTS gallery;
 CREATE TABLE IF NOT EXISTS gallery (
   GalleryID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
   Name      VARCHAR(50)            NOT NULL,
-  State    BOOLEAN                NOT NULL
+  State     BOOLEAN                NOT NULL
 );
 
 DROP TABLE IF EXISTS galleryimages;
 CREATE TABLE IF NOT EXISTS galleryimages (
-  GalleryID INTEGER 			NOT NULL,
-  ImageID   INTEGER             NOT NULL
+  GalleryID INTEGER NOT NULL,
+  ImageID   INTEGER NOT NULL
 );
 
-INSERT INTO images (ImageID, Type, PictureRef, Name, Caption) VALUES(1, 'jpg', 'harry-potter-and-the-chamber-of-secrets', 'Testimage' ,'');
-INSERT INTO gallery (GalleryID, Name, Status) VALUES(1, 'Testgallery', 0);
-INSERT INTO galleryimages (GalleryID, ImageID) VALUES(1, 1);
+INSERT INTO images (ImageID, Type, PictureRef, Name, Caption)
+VALUES (1, 'jpg', 'harry-potter-and-the-chamber-of-secrets', 'Testimage', '');
+INSERT INTO gallery (GalleryID, Name, State) VALUES (1, 'Testgallery', 0);
+INSERT INTO galleryimages (GalleryID, ImageID) VALUES (1, 1);
 
-INSERT INTO images (ImageID, Type, PictureRef, Name, Caption) VALUES(2, 'jpg', 'library-5', 'Library' ,'');
-INSERT INTO images (ImageID, Type, PictureRef, Name, Caption) VALUES(3, 'jpg', 'class-steel-installation', 'Class Steel Installation' ,'');
-INSERT INTO images (ImageID, Type, PictureRef, Name, Caption) VALUES(4, 'jpg', 'ArchitectureArtDesigns-180', 'Architecture Art Designs' ,'');
+INSERT INTO images (ImageID, Type, PictureRef, Name, Caption) VALUES (2, 'jpg', 'library-5', 'Library', '');
+INSERT INTO images (ImageID, Type, PictureRef, Name, Caption)
+VALUES (3, 'jpg', 'class-steel-installation', 'Class Steel Installation', '');
+INSERT INTO images (ImageID, Type, PictureRef, Name, Caption)
+VALUES (4, 'jpg', 'ArchitectureArtDesigns-180', 'Architecture Art Designs', '');
 
 ALTER TABLE galleryimages
-  Add Constraint PK_galleryimages
-PRIMARY KEY(GalleryID, ImageID),
-  Add Constraint FK_gallery
+  ADD CONSTRAINT PK_galleryimages
+PRIMARY KEY (GalleryID, ImageID),
+  ADD CONSTRAINT FK_gallery
 FOREIGN KEY (GalleryID)
-REFERENCES gallery(GalleryID),
-  Add Constraint FK_images
+REFERENCES gallery (GalleryID),
+  ADD CONSTRAINT FK_images
 FOREIGN KEY (ImageID)
-REFERENCES images(ImageID)
-;
+REFERENCES images (ImageID);
 
 DROP TABLE IF EXISTS messages;
 CREATE TABLE IF NOT EXISTS messages (
@@ -134,14 +142,15 @@ CREATE TABLE IF NOT EXISTS messages (
 
 DROP TABLE IF EXISTS pagecontent;
 CREATE TABLE IF NOT EXISTS pagecontent (
-  PageID    INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY ,
-  PageName  VARCHAR(50)            NOT NULL,
-  Headline  VARCHAR(255)           NOT NULL,
-  Content   LONGTEXT               NOT NULL
+  PageID   INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  PageName VARCHAR(50)            NOT NULL,
+  Headline VARCHAR(255)           NOT NULL,
+  Content  LONGTEXT               NOT NULL
 );
 
-ALTER TABLE pagecontent AUTO_INCREMENT = 0;
+ALTER TABLE pagecontent
+  AUTO_INCREMENT = 0;
 INSERT INTO pagecontent
-VALUES(DEFAULT ,"about", "About", "Test Content"),
-  (DEFAULT , "contact", "Contact Us", "Test Content"),
-  (DEFAULT , "home", "Home", "Test Content");
+VALUES (DEFAULT, "about", "About", "Test Content"),
+  (DEFAULT, "contact", "Contact Us", "Test Content"),
+  (DEFAULT, "home", "Home", "Test Content");
