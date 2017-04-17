7c9812a1c8e3b4db00e84fd786712dbe7d12798e
Schulzi66 <marius.schulze@t-online.de>
2017-04-17 16:03:20 +0100

    fixed db entry

--- sql/CreateDatabase.sql
+++ sql/CreateDatabase.sql
@@ -144,4 +144,4 @@ ALTER TABLE pagecontent AUTO_INCREMENT = 0;
 INSERT INTO pagecontent
 VALUES(DEFAULT ,"about", "About", "Test Content"),
   (DEFAULT , "contact", "Contact Us", "Test Content"),
-  (DEFAULT , "home", "Home", "Test Content");
+  (DEFAULT , "index", "Home", "Test Content");
