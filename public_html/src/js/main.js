df0bce61cac05a0152a2e850cf0adbfa26217f22
Julian <julian.christer@web.de>
2017-04-17 10:28:50 +0200

    image picker initialising, further development for image gallery creation

--- public_html/src/js/main.js
+++ public_html/src/js/main.js
@@ -9,14 +9,27 @@ $(document).ready(function () {
     }).parent().addClass('active').parent().parent().addClass('active');
 
 
-    $('#imageGalleryDropdown').on('change', function () {
+    /*$('#imageGalleryDropdown').on('change', function () {
         if (this.value == '1') {
-            //
+            alert("test");
         }
+    });*/
+
+    $('#imageGalleryDropdown a').on('click', function(e){
+        e.preventDefault();
+        $('#imageSelectionWrapper').show();
     });
 
-});
+// Initialize the object
+    $("select").imagepicker({
+        hide_select : false
+    })
+    // Retrieve the picker
+    $("select").data('picker');
 
+
+});
+//TODO Never use alert in productive code
 function validate() {
     if (document.forms["searchForm"]["bookTitle"].value === "" && document.forms["searchForm"]["bookAuthor"].value === "") {
         alert("Please fill either field with text for a valid search");
