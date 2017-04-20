f4a4026d468862b1cb817f6cde0dc99d989b3cf0
Darlith <rezzikoep@web.de>
2017-04-20 20:50:12 +0100

    breadcrumbs

--- public_html/protected/view/parts/breadcrumb.php
+++ public_html/protected/view/parts/breadcrumb.php
@@ -11,19 +11,33 @@
     echo '<a href="' . $root . '/index.php">';
     echo 'Home';
     echo '</a></li>';
-
-    foreach ($crumbs as $crumb) {
-        //TODO all: change this damn workaround
-        $counter++;
-        if ($counter >= 6) {
-            if ($crumb == "") {
-                $crumb = "Home";
-            }
-            echo '<li class="breadcrumb-item">';
-            echo '<a href="' . $base . '/' . $crumb . '">';
-            echo ucfirst(str_replace(array(".php", "_"), array("", " "), $crumb) . ' ');
-            echo '</a></li>';
-        }
+    //Any protected view has to be called from the dashboard, except for the dashboard itself.
+    if($crumbs[3] == "protected" && $crumbs[5] != "dashboard.php")
+    {
+        echo '<li class="breadcrumb-item">';
+        echo '<a href="' . $base . '/dashboard.php">';
+        echo 'Dashboard';
+        echo '</a></li>';
     }
+    echo '<li class="breadcrumb-item">';
+    echo '<a href="' . $base . '/' . end($crumbs) . '">';
+    echo ucfirst(str_replace(array(".php", "_"), array("", " "), end($crumbs)) . ' ');
+    echo '</a></li>';
+
+//    foreach ($crumbs as $crumb) {
+//        //TODO all: change this damn workaround
+//        $counter++;
+//        if ($counter >= 6) {
+//            if ($crumb == "") {
+//                $crumb = "Home";
+//            }
+//            echo '<li class="breadcrumb-item">';
+//            echo '<a href="' . $base . '/' . end($crumbs) . '">';
+//            echo ucfirst(str_replace(array(".php", "_"), array("", " "), end($crumbs)) . ' ');
+//            echo '</a></li>';
+//        }
+//    }
+
+
     ?>
 </ol>
