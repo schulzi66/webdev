7c677aef6af1ec2f26b9e2a085a248181e41fb97
Schulzi66 <marius.schulze@t-online.de>
2017-05-17 14:42:30 +0100

    fixed search and picture loading

--- public/view/searchresults.php
+++ public/view/searchresults.php
@@ -1,18 +1,18 @@
 <?php
-$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-require_once "$root/webdev/public_html/protected/entities/Book.php";
-require_once "$root/webdev/public_html/protected/controller/ImageGalleryController.php";
+//$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once __DIR__ . "/../../protected/entities/Book.php";
+require_once __DIR__ . "/../../protected/controller/ImageGalleryController.php";
 $books = unserialize($_GET["result-books"]);
 $count = Count($books); ?>
 <html>
 <head>
-    <?php include '../../protected/view/parts/head.php'; ?>
+    <?php include __DIR__ . '/parts/head.php'; ?>
 </head>
-<?php include '../../protected/view/parts/header.php'; ?>
+<?php include __DIR__ . '/parts/header.php'; ?>
 <body>
 <!-- This page shows a view of all books returned in either the "Search library" search or the main page search-->
 <div class="container">
-<?php include '../../protected/view/parts/breadcrumb.php';?>
+<?php include __DIR__ . '/parts/breadcrumb.php';?>
     <div class="panel panel-default">
         <div class="panel-heading"><?php echo $count ?> results returned</div>
         <table class="table table-hover table-bordered">
@@ -31,7 +31,7 @@ $count = Count($books); ?>
                 echo '<tr>';
                 echo '<td class="align-center">'
                     ?>
-                    <img class="img-thumbnail" width="100" height="80" src="../src/img/<?php echo $image[0][2] ?>.jpg"
+                    <img class="img-thumbnail" width="100" height="80" src="../../assets/img/<?php echo $image[0][2] ?>.jpg"
                          alt="<?php $image[0][2]; ?>">
                     <?php
                 echo '</td>';
@@ -52,7 +52,7 @@ $count = Count($books); ?>
             ?>
 
             <!-- Keep this at the end of the body tag to load the scripts at the right time -->
-            <?php include '../../protected/view/parts/scripts.php'; ?>
+            <?php include __DIR__ . '/parts/scripts.php'; ?>
 </body>
 <?php //include '../../protected/view/parts/footer.php'; ?>
 </html>
