644febbc014ff0dc7c9f859df1b9afcd9c3304fb
MariusSchulze <my_mail1980@web.de>
2017-05-18 00:04:11 +0100

    assets

--- public/view/parts/head.php
+++ public/view/parts/head.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/juls1337/webdev@v1.3/public/assets/css/main.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/juls1337/webdev@v1.3/public/assets/lib/css/image-picker.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/juls1337/webdev@v1.3/public/assets/lib/css/jquery-ui.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/juls1337/webdev@v1.3/public/assets/lib/css/jquery-ui.theme.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/juls1337/webdev@v1.3/public/assets/lib/css/jquery-ui.structure.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MariusSchulze/assets@v1.3/css/main.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MariusSchulze/assets@v1.3/lib/css/image-picker.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MariusSchulze/assets@v1.3/lib/css/jquery-ui.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MariusSchulze/assets@v1.3/lib/css/jquery-ui.theme.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MariusSchulze/assets@v1.3/lib/css/jquery-ui.structure.min.css">
